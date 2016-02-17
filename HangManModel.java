1d715f62a2e14256cf0a169172014ce33beb8d41
Vinay <vinay.more89@gmail.com>
2016-02-16 23:00:06 -0500

    Create HangManModel.java

---
+++ HangManModel.java
@@ -0,0 +1,26 @@
+
+/* 
+ * HangManModel.java 
+ *  
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * This program is Model part of Multiplayer HangMan game.
+ *
+ * @author Vinay Vasant More
+ *
+ */
+
+public class HangManModel {
+	static String players[] = new String[10];
+	static int score[] = new int[10];
+	static Random rword = new Random();
+	static List<String> WordList = new ArrayList<String>();
+
+	HangManModel() {
+	}
+}
